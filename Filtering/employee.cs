18305ae97553042a2fcb6c21754572008c0b16a6
Amr Mohamed Shawky <amrshawky936@gmail.com>
2025-07-26 18:48:34 +0300

    Add Projection&Filtering project and LINQ examples
    - Updated solution file to include new project "Projection&Filtering" with Debug and Release configurations.
    - Modified Program.cs to add comments on LINQ functionalities and removed the "Hello, World!" message.
    - Added employee.cs to define an Employee class implementing IEnumerable<Employee> for employee enumeration.
    - Enhanced Program.cs to demonstrate LINQ filtering and projection with a list of Employee objects.
    - Created Projection&Filtering.csproj targeting .NET 8.0 with necessary properties for implicit usings and nullable reference types.

---
+++ Filtering/employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectionAndFiltering
+{
+    public class Employee : IEnumerable<Employee>
+    {
+        private static List<Employee> _employees = new List<Employee>();
+        public Employee()
+        {
+            _employees.Add(this);
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public int Age { get; set; }
+        public string Department { get; set; }
+
+        public IEnumerator GetEnumerator()
+        {
+            return _employees.GetEnumerator();
+        }
+
+        IEnumerator<Employee> IEnumerable<Employee>.GetEnumerator()
+        {
+            return _employees.GetEnumerator();
+        }
+    }
+}
--- LINQWorks/Program.cs
+++ LINQWorks/Program.cs
@@ -2,9 +2,41 @@
 {
     internal class Program
     {
+            /*
+              * Introduction
+              * Projection & Filtering (select, where, OfType)
+              * Sorting (OrderBy, OrderByDescending, ThenBy, ThenByDescending, Reverse)
+              * Quantifiers (All, Any, Contains)
+              * Partitioning (Skip, SkipWhile, Take, TakeWhile, Chunk)
+              * Set Operations (Distinct, DistinctBy, Except, ExceptBy, Intersect, IntersectBy, Union, UnionBy)
+              * Joins (Join, GroupJoin)
+              * Grouping (GroupBy, ToLookup)
+            */
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+            // methode syntax 
+            // query expression syntax
+
+
+
+
+
+
+
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
